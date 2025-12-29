33a10217bf4e4f4b11330bbd24bf720083f64b1d
Tuann <tuntrungnguyen@gmail.com>
2025-12-29 11:10:29 +0700

    first commit

---
+++ api/index.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='../templates')
+
+def tinh_toan_kwp(loai_hinh, tien_dien):
+    # --- KHU VỰC CÔNG THỨC (Sẽ sửa lại sau) ---
+    he_so = 0
+    if loai_hinh == 'can_ho':
+        he_so = 0.0005 
+    elif loai_hinh == 'kinh_doanh':
+        he_so = 0.0004
+    elif loai_hinh == 'san_xuat':
+        he_so = 0.0003
+    
+    kwp = tien_dien * he_so
+    return round(kwp, 2)
+    # ------------------------------------------
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    ket_qua = None
+    du_lieu_nhap = {}
+
+    if request.method == 'POST':
+        try:
+            loai_hinh = request.form.get('loai_hinh')
+            tien_dien = float(request.form.get('tien_dien'))
+            du_lieu_nhap = {'loai_hinh': loai_hinh, 'tien_dien': tien_dien}
+            ket_qua = tinh_toan_kwp(loai_hinh, tien_dien)
+        except ValueError:
+            ket_qua = "Lỗi nhập liệu"
+
+    return render_template('index.html', ket_qua=ket_qua, du_lieu_nhap=du_lieu_nhap)
+
+if __name__ == '__main__':
+    app.run(debug=True)
